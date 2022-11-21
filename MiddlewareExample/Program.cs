b840a7dc07977d0292ec6deda2bd24add6709992
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-21 16:36:55 +0100

    Custom Middleware Class

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -1,23 +1,23 @@
+using MiddlewareExample.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
 // Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello ");
+    await context.Response.WriteAsync("\nFrom Middleware 1");
     await next(context);
 });
 
-// Middleware 1
-/*app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync("Hello again");
-});*/
+// Middleware 2
+app.UseMiddleware<MyCustomMiddleware>();
 
-// Middleware 1
+// Middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Hello again");
+    await context.Response.WriteAsync("\nFrom Middleware 3");
 });
 
 app.Run();
