ebf57ab1520b3db6cef86c4cb411032465522e28
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-21 15:59:08 +0100

    Middleware Chain

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -1,11 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Run(async(HttpContext context) => {
-    await context.Response.WriteAsync("Hello");
+// Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello ");
+    await next(context);
 });
 
-app.Run(async (HttpContext context) => {
+// Middleware 1
+/*app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello again");
+});*/
+
+// Middleware 1
+app.Run(async (HttpContext context) =>
+{
     await context.Response.WriteAsync("Hello again");
 });
 
