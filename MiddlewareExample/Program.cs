cb3a81080ff9b5fad13b7c95897e32dc3606aa46
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-21 15:42:46 +0100

    Run

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async(HttpContext context) => {
+    await context.Response.WriteAsync("Hello");
+});
+
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello again");
+});
 
 app.Run();
