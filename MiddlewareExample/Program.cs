8b51d63f34e7928011a45786ebc6a5ced1d2803c
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-22 15:48:11 +0100

    Custom Conventional Middleware Class

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -13,7 +13,8 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
 
 // Middleware 2
 // app.UseMiddleware<MyCustomMiddleware>();
-app.UseMyCustomMiddleware();
+// app.UseMyCustomMiddleware();
+app.UseHelloCustomMiddleware();
 
 // Middleware 3
 app.Run(async (HttpContext context) =>
